a79764a4207fe895dbc2ea7636131a0d831a8953
Eduardo Coutinho Idalgo <eduardoidalgo7@gmail.com>
2026-07-16 19:44:52 -0300

    Controller criada

---
+++ src/main/java/dev/idaaalgo/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.idaaalgo.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa API";
+    }
+}
