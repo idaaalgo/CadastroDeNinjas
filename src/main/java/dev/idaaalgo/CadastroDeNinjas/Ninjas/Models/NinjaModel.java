5cb0f14092f34cc89123bd678f6859a4d7fb430c
Eduardo Coutinho Idalgo <eduardoidalgo7@gmail.com>
2026-07-17 02:48:03 -0300

    fix: Coloca coluna de e-mail como unique

--- src/main/java/dev/idaaalgo/CadastroDeNinjas/Ninjas/Models/NinjaModel.java
+++ src/main/java/dev/idaaalgo/CadastroDeNinjas/Ninjas/Models/NinjaModel.java
@@ -16,7 +16,10 @@ public class NinjaModel {
     private Long id;
 
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
 
     @ManyToOne // Many = ninjas / ToOne = missao
